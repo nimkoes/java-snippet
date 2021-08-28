5c02b3c1e947cb2ca73586ff4beeb80d15ca08a4
nimkoes <xxxelppa@gmail.com>
2021-08-28 22:22:58 +0900

    mod Student class

--- src/com/tistory/xxxelppa/section_002/Student.java
+++ src/com/tistory/xxxelppa/section_002/Student.java
@@ -3,14 +3,24 @@ package com.tistory.xxxelppa.section_002;
 public class Student {
     public enum Gender {MALE, FEMALE}
 
+    public enum City {ANYANG, SEOUL}
+
     private String name;
     private int score;
     private Gender gender;
+    private City city;
 
     public Student(String name, int score, Gender gender) {
         this.name = name;
         this.score = score;
         this.gender = gender;
+    }
+
+    public Student(String name, int score, Gender gender, City city) {
+        this.name = name;
+        this.score = score;
+        this.gender = gender;
+        this.city = city;
     }
 
     public String getName() {
@@ -23,5 +33,9 @@ public class Student {
 
     public Gender getGender() {
         return gender;
+    }
+
+    public City getCity() {
+        return city;
     }
 }
