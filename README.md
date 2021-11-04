d21523c07b43e919442bae3b0d7c68adf25eb7a5
nimkoes <xxxelppa@gmail.com>
2021-11-04 09:20:33 +0900

    builder 패턴 실습  - static inner class 사용

--- README.md
+++ README.md
@@ -7,15 +7,13 @@
   
   - [section_001 : Auto Boxing 성능 테스트][link_section_001]
   - [section_002 : Stream Collectors 실습][link_section_002]
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  - [section_003 : Builder Pattern 실습][link_section_003]
+
+　  
+　  
+　  
+
+
 [link_section_001]:https://github.com/nimkoes/java-snippet/blob/master/src/me/nimkoes/section_001/Main.java
 [link_section_002]:https://github.com/nimkoes/java-snippet/blob/master/src/me/nimkoes/section_002/StreamCollectors.java
+[link_section_003]:https://github.com/nimkoes/java-snippet/blob/master/src/me/nimkoes/section_003/SampleData.java
--- src/me/nimkoes/section_003/SampleData.java
+++ src/me/nimkoes/section_003/SampleData.java
@@ -6,4 +6,63 @@ public class SampleData {
     private String gender;
     private String address;
     private String desc;
+
+    // builder 는 static 클래스로 내부에 구현하는 것이 일반적
+    public static class Builder {
+        // 필수값
+        private final String name;
+        private final Integer age;
+
+        // 선택값
+        private String gender;
+        private String address;
+        private String desc;
+
+        public Builder(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Builder gender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder desc(String desc) {
+            this.desc = desc;
+            return this;
+        }
+
+        // outer class 인 SampleData 클래스 객체를 반환하는 builder 메소드
+        public SampleData build() {
+            return new SampleData(this);
+        }
+    }
+
+    // inner static class 를 전달 받는 private constructor 정의
+    private SampleData(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+
+        this.gender = builder.gender;
+        this.address = builder.address;
+        this.desc = builder.desc;
+    }
+
+    // 값을 확인해보기 위해 toString override
+    @Override
+    public String toString() {
+        return "SampleData{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
 }
