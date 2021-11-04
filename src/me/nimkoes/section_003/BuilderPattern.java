b9c645f58232fc788fb1fb364737e1746a55e9da
nimkoes <xxxelppa@gmail.com>
2021-11-04 09:07:02 +0900

    builder 패턴 실습, 데이터 클래스 생성ㅜ

---
+++ src/me/nimkoes/section_003/BuilderPattern.java
@@ -0,0 +1,7 @@
+package me.nimkoes.section_003;
+
+public class BuilderPattern {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/me/nimkoes/section_003/SampleData.java
@@ -0,0 +1,9 @@
+package me.nimkoes.section_003;
+
+public class SampleData {
+    private String name;
+    private Integer age;
+    private String gender;
+    private String address;
+    private String desc;
+}
